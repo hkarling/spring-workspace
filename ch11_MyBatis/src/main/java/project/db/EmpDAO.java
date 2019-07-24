f3852947022446d3cd6a0535cf44efd12d6ca6c7
hkarling <dantert@gmail.com>
2019-07-24 14:06:26 +0900

    07241406

--- ch11_MyBatis/src/main/java/project/db/EmpDAO.java
+++ ch11_MyBatis/src/main/java/project/db/EmpDAO.java
@@ -1,7 +1,10 @@
 package project.db;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ibatis.session.RowBounds;
 import org.apache.ibatis.session.SqlSession;
 
 import project.dto.EmpDTO;
@@ -73,6 +76,111 @@ public class EmpDAO {
 		try {
 			session = DbUtil.getSqlSession();
 			state = (session.insert("empMapper.empUpdate", empDTO) > 0) ? true : false;
+		} finally {
+			DbUtil.sessionClose(session, state);
+		}
+	}
+
+	/**
+	 * 수정하기 2
+	 */
+	public void updateByJob(EmpDTO empDTO, String job) {
+		SqlSession session = null;
+		boolean state = false;
+		try {
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("dto", empDTO);
+			map.put("job", job);
+			session = DbUtil.getSqlSession();
+			state = (session.insert("empMapper.updateByJob", map) > 0) ? true : false;
+		} finally {
+			DbUtil.sessionClose(session, state);
+		}
+	}
+
+//////////////////////////////////////////////////////////////////////////////////////////
+	/**
+	 * 전체검색
+	 */
+	public void empSelectAll() {
+		SqlSession session = null;
+		try {
+			session = DbUtil.getSqlSession();
+			RowBounds rb = new RowBounds(0, 5);
+			List<EmpDTO> list = session.selectList("empSelectMapper.selectAll", null, rb);
+
+			for (EmpDTO dto : list) {
+				System.out.println(dto);
+			}
+		} finally {
+			DbUtil.sessionClose(session);
+		}
+	}
+
+	/**
+	 * 사원번호에 해당하는 검색
+	 */
+	public void empSelectByempno(int empno) {
+		SqlSession session = null;
+		try {
+			session = DbUtil.getSqlSession();
+			EmpDTO dto = session.selectOne("empSelectMapper.selectByEmpNo", empno);
+
+			System.out.println(dto);
+		} finally {
+			DbUtil.sessionClose(session);
+		}
+	}
+
+	/**
+	 * sal의 최소 ~ 최대 검색
+	 */
+	public void empSelectBySal(int min, int max) {
+		SqlSession session = null;
+		boolean state = false;
+		try {
+			Map<String, Integer> map = new HashMap<String, Integer>();
+			map.put("min", min);
+			map.put("max", max);
+			session = DbUtil.getSqlSession();
+			List<EmpDTO> list = session.selectList("empSelectMapper.selectBySal", map);
+			for (EmpDTO dto : list) {
+				System.out.println(dto);
+			}
+		} finally {
+			DbUtil.sessionClose(session, state);
+		}
+	}
+
+	/**
+	 * 급여보다 더 적게 받는 사원 검색
+	 */
+	public void empSelectBySal2(int sal) {
+		SqlSession session = null;
+		boolean state = false;
+		try {
+			session = DbUtil.getSqlSession();
+			List<EmpDTO> list = session.selectList("empSelectMapper.selectBySal2", sal);
+			for (EmpDTO dto : list) {
+				System.out.println(dto);
+			}
+		} finally {
+			DbUtil.sessionClose(session, state);
+		}
+	}
+	
+	/**
+	 * 특정 컬럼을 기준으로 정렬하기
+	 */
+	public void empSelectByOrder(String column) {
+		SqlSession session = null;
+		boolean state = false;
+		try {
+			session = DbUtil.getSqlSession();
+			List<EmpDTO> list = session.selectList("empSelectMapper.selectByOrder", column);
+			for (EmpDTO dto : list) {
+				System.out.println(dto);
+			}
 		} finally {
 			DbUtil.sessionClose(session, state);
 		}
--- ch11_MyBatis/src/main/java/project/db/MainApp.java
+++ ch11_MyBatis/src/main/java/project/db/MainApp.java
@@ -11,13 +11,24 @@ public class MainApp {
 //		dao.selectAll();
 //		System.out.println("----- 사원 등록 -----");
 //		dao.insert(new EmpDTO(9898,"김미영", "팀장", 4000));
-		dao.selectAll();
-		System.out.println("----- 사원 수정 -----");
-		dao.update(new EmpDTO(9898, "어이이", "아무것",5000));
-		dao.selectAll();
-		System.out.println("----- 사원 삭제 -----");
-		dao.delete(9898);
-		dao.selectAll();
-		
+//		dao.selectAll();
+//		System.out.println("----- 사원 수정 -----");
+//		dao.update(new EmpDTO(9898, "어이이", "아무것",5000));
+//		dao.selectAll();
+//		System.out.println("----- 사원 삭제 -----");
+//		dao.delete(9898);
+//		dao.selectAll();
+//		dao.updateByJob(new EmpDTO(9898,"형님", "실직", 0), "팀장");
+//		dao.selectAll();		
+//		System.out.println("----- 전체 사원 검색 -----");
+//		dao.empSelectAll();
+//		System.out.println("----- 사원 번호 검색 -----");
+//		dao.empSelectByempno(7369);
+//		System.out.println("----- 범위 검색 -----");
+//		dao.empSelectBySal(3000, 10000);
+		System.out.println("----- sal 얼마 이하 검색 -----");
+		dao.empSelectBySal2(3000);
+		System.out.println("----- 컬럼명으로 정렬 -----");
+		dao.empSelectByOrder("sal");
 	}
 }
