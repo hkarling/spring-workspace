b8487b872f88658ddf40795af517cd9741e00656
hkarling <dantert@gmail.com>
2020-01-06 21:55:19 +0900

    refactor: Review Chapter03 AOP

---
+++ step03_AOP/src/main/java/aop/exam/MessageServiceImpl.java
@@ -0,0 +1,57 @@
+package aop.exam;
+
+import org.springframework.aop.framework.AopContext;
+import org.springframework.stereotype.Service;
+
+@Service("target")
+public class MessageServiceImpl implements MessageService {
+
+	public void KorHello() {
+		System.out.println("MessageServiceImpl의 KorHello()의 핵심기능이다.");
+		try {
+			System.out.println("*****");
+			/**
+			 * advice를 적용할 타겟대상을 직접호출하면 advice 적용이 안된다.
+			 * proxyServer를 구해서 proxy가 호출할 수 있도록 한다.
+			 */		
+			MessageService se = (MessageService) AopContext.currentProxy();
+			se.engHello("RJW...");
+			
+//			this.engHello("껒..");
+
+			System.out.println("*****");
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void engHello(String name) {
+		System.out.println("MessageServiceImpl의 engHello(String name)의 핵심기능이다.");
+		try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String hello() {
+		System.out.println("MessageServiceImpl의 hello()의 핵심기능이다.");
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return "안녕하냐고오오오";
+	}
+
+	public int hello(String name) {
+		System.out.println("MessageServiceImpl의 hello(String name)의 핵심기능이다.");
+		try {
+			Thread.sleep(100);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return 49;
+	}
+}
